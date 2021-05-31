f35f29b8efecc83f69a8954b8c6f24bd92094cf8
Pierre Schnizer <pierre.schnizer@helmholtz-berlin.de>
2021-05-31 10:14:20 +0200

    Commented bpm_data: arrange as xarray
    The function bpm_data simplifes exporting bpm data to
    a xarray. Added further comments for explaining the function

--- bact_archiver_bessyii/utils/fofb_bpm_data.py
+++ bact_archiver_bessyii/utils/fofb_bpm_data.py
@@ -25,8 +25,8 @@ def bpm_data(archiver: ArchiverInterface,
         t1:        end time of interval
         bpm_names: names of the beam position monitors
 
-    This function retrieves the bpm data from the archiver. It uses the names used
-    at BESSY II archiver.
+    This function retrieves the bpm data from the archiver. It
+    uses the names used at BESSY II archiver.
 
     * x plane
     * y plane
@@ -38,36 +38,54 @@ def bpm_data(archiver: ArchiverInterface,
         other instance, read access also locks).
     '''
 
-    # Get the dataframes
+    # Get the dataframes for the different variables
     kwargs = dict(t0=t0, t1=t1, time_format='datetime')
     bpm_x = archiver.getData('FOFBCC:fastBpmX', **kwargs)
     bpm_y = archiver.getData('FOFBCC:fastBpmY', **kwargs)
     bpm_state = archiver.getData('FOFBCC:fastStateBpm', **kwargs)
 
+    # Currently EPICS 3.x is used at BESSY II. Thus the data are
+    # not necessarily synchronous. I assume here that the one which
+    # is longer contains a few extra entries.
     if bpm_x.shape[0] < bpm_y.shape[0]:
         index_to_use = bpm_x.index
     else:
         index_to_use = bpm_y.index
     n_rows = len(index_to_use)
 
+    # Now let's create the array. I use the bpm names as index for
+    # the 'bpm' dimension
     data = xr.DataArray(
         dims=['time', 'bpm', 'plane'],
         coords=[index_to_use.values, bpm_names, ['x', 'y']],
         name='bpm_data'
     )
+
+    # bpm_x and bpm_y are indexed by time stamps. xarray takes care
+    # of sorting them to the right place. I did not yet experience
+    # that the longer vector did not contain indices the shorter
+    # vector would have
     data.loc[dict(plane='x')] = bpm_x[:n_rows]
     data.loc[dict(plane='y')] = bpm_y[:n_rows]
 
+    # Not all bpm's are active. Let's use the state array to find
+    # out which these are. The dimension 'time2' is intentional as
+    # the state is changed much more seldom than data arrive from
+    # the fast orbit feedback. Thus these dimension are not the
+    # same ones
     state = xr.DataArray(
         data=bpm_state.astype(np.bool), dims=['time2', 'bpm'],
         coords=[bpm_state.index.values, bpm_names],
         name='bpm_state'
     )
 
+    # Select the first timestamp. I assume (but do not check) that
+    # the the state did not change during the export time range
     valid_bpm = state.isel(time2=0)
     bpmpos_a = xr.DataArray(dims=['bpm'], coords=[bpm_names], name='bpm_pos')
     bpmpos_a.loc[dict(bpm=valid_bpm)] = bpm_pos
 
+    # Combine measured and state data
     dataset = xr.Dataset(dict(data=data, state=state, pos=bpmpos_a))
     return dataset
 
